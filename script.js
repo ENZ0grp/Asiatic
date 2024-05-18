ada1fce28b50b0a5c914355bfed95c5f9816d3dd
Rohit Challa <rohit@enzogrp.com>
2024-05-17 20:43:27 -0400

    Add HTML, CSS, and JavaScript files

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
-document.getElementById('imageUploadForm').addEventListener('submit', function (event) {
+async function uploadAndProcessImage(file) {
+    const imageBase64 = await loadImageBase64(file);
+  
+    axios({
+      method: "POST",
+      url: "https://detect.roboflow.com/fire-vqbia/1",
+      params: {
+        api_key: "4KIuSPChxh5KWl6uBXnk",
+      },
+      data: imageBase64,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then(function (response) {
+        const imageUrl = response.data.prediction.results[0].output_url;
+        displayProcessedImage(imageUrl);
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
+  
+  async function loadImageBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result.split(",")[1]); // Extract base64 string
+      reader.onerror = (error) => reject(error);
+    });
+  }
+  
+  document.getElementById("imageUploadForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
   
-    const fileInput = document.getElementById('imageInput');
+    const fileInput = document.getElementById("imageInput");
     if (fileInput.files.length === 0) {
-      alert('Please select a file to upload.');
+      alert("Please select a file to upload.");
       return;
     }
   
     const file = fileInput.files[0];
-    const formData = new FormData();
-    formData.append('image', file);
-  
-    fetch('https://universe.roboflow.com/ltttnt/fire-vqbia/model/1', {
-      method: 'POST',
-      body: formData,
-    })
-      .then(response => response.json())
-      .then(data => {
-        const imageUrl = data.prediction.results[0].output_url;
-        displayProcessedImage(imageUrl);
-      })
-      .catch(error => {
-        console.error('Error processing image:', error);
-      });
+    uploadAndProcessImage(file);
   });
-  
-  function displayProcessedImage(imageUrl) {
-    const imagePreview = document.getElementById('imagePreview');
-    imagePreview.innerHTML = `<img src="${imageUrl}" alt="Processed Image" />`;
-  }
   
